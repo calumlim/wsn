5d9ee64738d53d45b320052f7c72febb8e426674
Calum Lim Sheng En <calumstah@gmail.com>
2018-10-07 20:03:39 +0800

    fixed receiving function

--- wsn.c
+++ wsn.c
@@ -2,10 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
+#include <unistd.h>
 #include "mpi.h"
 
 #define no_row 4
 #define no_col 5
+
+int *countAdjacent(int arr[], int n, int maxVal, int rank, int rand_val){
+  int count_arr[maxVal];
+  int final_arr[6];
+  memset(count_arr, 0, maxVal);
+
+  //Count the number of occurences for each random number
+  for (int i=0; i<n; i++){
+    count_arr[arr[i]]++;
+  }
+  for (int j=0; j<n; j++){
+    if (count_arr[arr[j]]>=3){
+      return count_arr[arr[j]];
+    }
+  }
+  return 0;
+}
 
 int main(argc, argv)
 int argc;
@@ -29,10 +48,13 @@ char **argv;
 
   if (rank!=20){
     srand(clock());
-    rand_val = rand()%10+1;
+    rand_val = rand()%5+1;
     nreq = 0;
+    double time_spent = 0;
+    clock_t begin_time = clock();
     rowpos = rank/no_col;
     colpos = rank%no_col;
+    memset(grid[rowpos][colpos], 0, sizeof(grid[rowpos][colpos]));
     //left
     if (colpos-1<0){
       //top left
@@ -177,7 +199,7 @@ char **argv;
       counter++;
 
     }
-    
+    MPI_Waitall(nreq, request, statuses);
     //Receive all adjacent random values
     if (counter>=3){
       printf("rank: %d  ", rank);
@@ -187,40 +209,55 @@ char **argv;
       if (counter==3){
         //top row case
         if (rowpos-1<0){
-          MPI_Irecv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-          printf("top row: (%d,%d,%d)\n", grid[rowpos][colpos][left],grid[rowpos][colpos][right],grid[rowpos][colpos][down]);
+          MPI_Recv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          clock_t end_time = clock();
+          time_spent = (double)(end_time - begin_time) / CLOCKS_PER_SEC;
+          countAdjacent(grid[rowpos][colpos], 4, 5);
+          //printf("top row: (%d,%d,%d)   |   time: %f\n", grid[rowpos][colpos][left],grid[rowpos][colpos][right],grid[rowpos][colpos][down], time_spent);
 
         //bottom row case
         } else if (rowpos+1>no_row-1){
-          MPI_Irecv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][left] , 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-          printf("bottom row: (%d,%d,%d)\n", grid[rowpos][colpos][left],grid[rowpos][colpos][right],grid[rowpos][colpos][top]);
+          MPI_Recv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][left] , 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          clock_t end_time = clock();
+          time_spent = (double)(end_time - begin_time) / CLOCKS_PER_SEC;
+          countAdjacent(grid[rowpos][colpos],4, 5);
+          //printf("bottom row: (%d,%d,%d)    | time: %f\n", grid[rowpos][colpos][left],grid[rowpos][colpos][right],grid[rowpos][colpos][top], time_spent);
 
         //left case
         } else if (colpos-1<0){
-          MPI_Irecv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][top] , 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-          printf("left row: (%d,%d,%d)\n", grid[rowpos][colpos][top],grid[rowpos][colpos][down],grid[rowpos][colpos][right]);
+          MPI_Recv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][top] , 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          clock_t end_time = clock();
+          time_spent = (double)(end_time - begin_time) / CLOCKS_PER_SEC;
+          countAdjacent(grid[rowpos][colpos],4, 5);
+          //printf("left row: (%d,%d,%d)   |   time: %f\n", grid[rowpos][colpos][top],grid[rowpos][colpos][down],grid[rowpos][colpos][right], time_spent);
 
         //right case
         } else if (colpos+1>no_col-1){
-          MPI_Irecv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-          MPI_Irecv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-          printf("right row: (%d,%d,%d)\n", grid[rowpos][colpos][top],grid[rowpos][colpos][down],grid[rowpos][colpos][left]);
+          MPI_Recv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          MPI_Recv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+          clock_t end_time = clock();
+          time_spent = (double)(end_time - begin_time) / CLOCKS_PER_SEC;
+          countAdjacent(grid[rowpos][colpos],4, 5);
+          //printf("right row: (%d,%d,%d)   |   time: %f\n", grid[rowpos][colpos][top],grid[rowpos][colpos][down],grid[rowpos][colpos][left], time_spent);
 
         }
       //When there is more than three adjacent nodes, counter = 4
-      } else {
-        MPI_Irecv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-        MPI_Irecv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
-        MPI_Irecv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-        MPI_Irecv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-        printf("middle row: (%d,%d,%d,%d)\n", grid[rowpos][colpos][left], grid[rowpos][colpos][right],grid[rowpos][colpos][top],grid[rowpos][colpos][down]);
+    } else if (counter>3){
+        MPI_Recv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
+        clock_t end_time = clock();
+        time_spent = (double)(end_time - begin_time) / CLOCKS_PER_SEC;
+        countAdjacent(grid[rowpos][colpos],4, 5);
+        //printf("middle row: (%d,%d,%d,%d)   |   time: %f\n", grid[rowpos][colpos][left], grid[rowpos][colpos][right],grid[rowpos][colpos][top],grid[rowpos][colpos][down], time_spent);
 
       }
     //Nodes with lesser than 2 adjacent nodes receiving their adjacent random numbers.
@@ -230,34 +267,32 @@ char **argv;
       //top left
       if (rowpos-1<0 && colpos-1<0){
         //right
-        MPI_Irecv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
         //bottom
-        MPI_Irecv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &request[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
 
       //top right
       } else if (rowpos-1<0 && colpos+1>no_col-1){
         //left
-        MPI_Irecv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
         //bottom
-        MPI_Irecv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &request[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][down], 1, MPI_INT, colpos+(no_col*(rowpos+1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
 
       //bottom left
       } else if (rowpos+1>no_row-1 && colpos-1<0){
         //right
-        MPI_Irecv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][right], 1, MPI_INT, colpos+1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
         //top
-        MPI_Irecv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &request[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
 
       //bottom right
       } else if (rowpos+1>no_row-1 && colpos+1>no_col-1){
         //left
-        MPI_Irecv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &request[nreq++]);
+        MPI_Recv(&grid[rowpos][colpos][left], 1, MPI_INT, colpos-1+(no_col*rowpos), 0, MPI_COMM_WORLD, &statuses[nreq++]);
         //top
-        MPI_Irecv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &request[nreq++]);
-
+        MPI_Recv(&grid[rowpos][colpos][top], 1, MPI_INT, colpos+(no_col*(rowpos-1)), 0, MPI_COMM_WORLD, &statuses[nreq++]);
       }
     }
-    MPI_Waitall(nreq, request, statuses);
   }
   MPI_Finalize();
   return 0;
